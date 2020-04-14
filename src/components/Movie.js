45b16175673fab87fedc0ad609f0d7e4db1f5840
TLau <nonotinthesameway@gmail.com>
2020-04-14 02:51:24 -0700

    added fav/watch later buttons to single movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getMovie } from "../utility/MovieAPI";
+import FavButton from "./FavButton";
+import WatchLaterButton from "./WatchLaterButton";
 
 const Movie = () => {
   let { id } = useParams();
@@ -22,6 +24,8 @@ const Movie = () => {
       <img src={movie.backdrop_path} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>{movie.overview}</p>
+      <FavButton movie={movie} />
+      <WatchLaterButton movie={movie} />
       <Link to="/">Back to Movies</Link>
     </div>
   );
