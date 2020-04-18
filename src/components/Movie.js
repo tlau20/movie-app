69641b2d18df24a6762849fcec522eb7139404f1
TLau <nonotinthesameway@gmail.com>
2020-04-17 21:51:53 -0700

    added genres and runtime to single movie view

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,12 +7,15 @@ import WatchLaterButton from "./WatchLaterButton";
 const Movie = () => {
   let { id } = useParams();
   const [movie, setMovie] = useState({});
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const movie = await getMovie(id).catch((error) => {
         console.log(error);
       });
+      const genres = movie.genres.map((genre) => genre.name);
+      setGenres(genres);
       setMovie(movie);
     };
 
@@ -24,6 +27,11 @@ const Movie = () => {
       <img src={movie.backdrop_path} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>{movie.overview}</p>
+      <p>{movie.runtime} min</p>
+      {/* <p>{movie.release_date}</p>
+      <p>{movie.budget}</p>
+      <p>{movie.revenue}</p> */}
+      <p>{genres.join(", ")}</p>
       <FavButton movie={movie} />
       <WatchLaterButton movie={movie} />
       <Link to="/">Back to Movies</Link>
