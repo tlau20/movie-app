d7b1277b61f02849ace86d920e11b164536058fe
TLau <nonotinthesameway@gmail.com>
2020-04-23 04:02:23 -0700

    hiding/showing header on scroll fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,17 @@ const Header = () => {
     }
   });
 
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+    } else {
+      document.getElementById("header").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
   return (
     <header id="header">
       <div className="logo">
